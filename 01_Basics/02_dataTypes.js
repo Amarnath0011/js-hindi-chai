272513b5800ecdf0d7a60318e5569d980ac7fbf8
Amarnath <amarnathkumar9305@gmail.com>
2025-03-25 07:01:58 +0000

    datatypes

---
+++ 01_Basics/02_dataTypes.js
@@ -0,0 +1,31 @@
+"use strict";// treat all JS code as newer version
+// alert ("hello"); // have different syntax in node js , i will run in browser console 
+
+console.log(20+
+    5); //code readability should be high 
+
+console.log("Amarnath");
+
+
+let name = "Amarnath"
+let age = 22
+let isLoggedIn = false
+
+//primitive datatype
+
+// number => 2 to the power 53
+// bigint =>
+// string =>" "
+//boolean => true /false
+//null -> standalone value
+//undefined => value is not assigned \
+//symbol -> unique 
+
+//object
+
+console.log(typeof "Amarnath") // string
+console.log(typeof age) //number 
+console.log(typeof null) //this gives object **for interview
+console.log(typeof undefined); //undefined 
+
+
