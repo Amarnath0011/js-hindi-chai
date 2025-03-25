31801bcf01a4765cfba8f2aed7785ddd9d468b70
Amarnath <amarnathkumar9305@gmail.com>
2025-03-25 10:07:09 +0000

    conversion and operation

--- 01_Basics/03_conversionOperation.js
+++ 01_Basics/03_conversionOperation.js
@@ -9,8 +9,8 @@ let score = true;
 
 let valueInNumber = Number(score) //conversion of string into number 
 
-console.log(typeof(valueInNumber))
-console.log(valueInNumber) //this will give a NaN ie not a number
+// console.log(typeof(valueInNumber))
+// console.log(valueInNumber) //this will give a NaN ie not a number
 
 
 //"33" => 33
@@ -38,5 +38,51 @@ let booleanIsLoggedIn= Boolean(isLoggedIn) //convert into boolean
 
 let someNumber= 33;
 let stringNumber = String(someNumber) //convert number into string 
-console.log(typeof stringNumber)
-console.log(stringNumber)
+// console.log(typeof stringNumber)
+// console.log(stringNumber)
+
+
+// ************** Operations ********************
+
+let value = 3
+let negValue  = -value
+// console.log(negValue)
+// console.log(2+2)
+// console.log(2-2)
+// console.log(2*2)
+// console.log(2**3)
+// console.log(2/3)
+// console.log(2%3)
+
+let str1="Hello"
+let str2 = " Amarnath"
+
+let str3 = str1+str2
+
+/*console.log(str3) //Hello Amarnath  
+console.log("1"+2) //12
+console.log(1+"2"); //12
+console.log("1"+3+4) // 134
+console.log(1+4+"6") //56 */
+
+// console.log(+true); //1
+// console.log(true+"") // true
+// console.log(+"")//0
+
+
+let num1,num2,num3;
+num1=num2=num3=2+3;
+console.log(num1,num2,num3)
+
+let gameCounter=100;
+gameCounter++;
+console.log(gameCounter);
+
+
+
+let gameCounter2=100;
+++gameCounter;
+console.log(gameCounter2);
+
+//
+
