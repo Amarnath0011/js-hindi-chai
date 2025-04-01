ad42c4a2148ba683017111c6ea8c3e5896ae0403
Amarnath <amarnathkumar9305@gmail.com>
2025-04-01 14:04:55 +0000

    high order in array

---
+++ 05_iterations/four.js
@@ -0,0 +1,17 @@
+const myObj = {
+    js: 'javaScript',
+    cpp :'c++',
+    rb : 'ruby',
+    swift:'swify by apple '
+
+}
+
+for(const key in myObj){
+    console.log(`${key} shortcut for ${myObj[key]}`)
+}
+
+const programming = ["js", "rb","py","java","cpp"]
+
+for (const key in programming) {
+        console.log(programming[key])
+    }
