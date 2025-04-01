927c2b357e87d916f1cdc218be3141b823e6d656
Amarnath <amarnathkumar9305@gmail.com>
2025-04-01 13:55:07 +0000

    for of loop in js

---
+++ 05_iterations/one.js
@@ -0,0 +1,24 @@
+// for loop 
+  
+for (let i = 0; i <= 10; i++) {
+    console.log(i)
+}
+
+// for (let i = 0; i <= 10; i++) {
+//     console.log(`outerloop value :${i}`)
+//     for (let j = 0; j <= 10; j++) {
+//         console.log(`inner loop :${j}`)
+//     }
+    
+// }
+
+let myArray = ["flash","batman","superman"]
+for (let index = 0; index < myArray.length; index++) {
+    const element = myArray[index];
+    //  console.log(element)
+}
+
+//keywords
+
+// break
+//continue
---
+++ 05_iterations/three.js
@@ -0,0 +1,45 @@
+// for of loop(Array specific loop )
+
+["","",""]
+[{},{},{}]
+
+const arr = [1,2,3,4,5]
+
+for (const num of arr) {// this object is not object of js
+    console.log(num);
+    
+}
+const greeting = "Hellow world!"
+for (const greet of greeting) {
+    if(greet==" "){
+        continue;
+    }
+    // console.log( `Each char is ${greet}`)
+}
+
+//Maps(Object containing as key and value pair , remember insertion order of the key , it is unique in the maps collection)
+
+const map = new Map()
+map.set('IN',"india")
+map.set('US',"United state of America")
+map.set('Uk',"United Kingdom")
+map.set('IN',"india")//this will not add as it contains only unique value
+
+
+console.log(map)
+// for (const key of map) {
+//     console.log(key)
+// }
+
+for(const [key,value] of map){
+    console.log(key,':-' , value);
+}
+
+// const myObj = {
+//     game1 : "NFS",
+//     game2 : "BGMI",
+//     game3 : "valorant". //Object is not iterable
+// }
+// for (const element of myObj) {
+//     console.log(myObj)
+// }
