c05508f598629364ded43b92384894d804704e59
Amarnath <amarnathkumar9305@gmail.com>
2025-04-02 08:47:40 +0000

    for Each in js

--- 05_iterations/five.js
+++ 05_iterations/five.js
@@ -1,7 +1,36 @@
-const programming = ["js", "rb","py","java","cpp"]
+const coding = ["js", "rb","py","java","cpp"]
 
-programming.forEach( function (item){
-    console.log(item)
-} )
+// programming.forEach( function (item){
+//     console.log(item)
+// } )
 
-programming.forEach(()=>{console.log(item)})
+// programming.forEach(()=>{console.log(item)})
+
+
+// function printMe(item){
+//     console.log(item)
+// }
+// coding.forEach(printMe)
+
+
+// coding.forEach((item,index,arr)=>{console.log(item,index,arr)})
+
+const myCoding = [
+
+    {
+        languageName: "javascript",
+        languageFileName : "js"
+    }
+    ,
+    {
+        languageName: "java",
+        languageFileName: "java",
+    },
+    {
+        languageName: "c language",
+        languageFileName : "c"
+    }
+]
+
+myCoding.forEach((item)=>{console.log(item.languageFileName)})
+myCoding.forEach((item)=>{console.log(item.languageName)})
