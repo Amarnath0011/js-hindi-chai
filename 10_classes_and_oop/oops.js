ca984f72fa6fe254322f985e38141a9e6fb469e5
Amarnath <amarnathkumar9305@gmail.com>
2025-04-16 20:33:05 +0000

    oops in js

---
+++ 10_classes_and_oop/oops.js
@@ -0,0 +1,16 @@
+// const user = {
+//     username: "hitesh",
+//     loginCount : 8,
+//     signedIn :true,
+//     getUserDetail:function(){
+//         console.log("Got user detail from database")
+//         console.log(`username: ${this.username}`)
+//         console.log(this)
+//     }
+// }
+// console.log(user.username)
+// user.getUserDetail()
+
+const promiseOne = new Promise(function(resolve,reject){
+    
+})
